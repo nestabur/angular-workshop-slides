fe912af77159805bd67d93c6a9ac1eb94a100f00
Nestor Tarin Burriel <neb@ti8m.ch>
2016-06-16 12:24:04 +0200

    fixed wrong setup steps

--- code/02-project/setup.sh
+++ code/02-project/setup.sh
@@ -1,9 +1,10 @@
 git clone https://github.com/nestabur/address-book-service
 cd address-book-service
 npm install
+npm install -g strongloop
 slc run
 
-git clone https://github.com/matoilic/AngularWorkshop
+git clone https://github.com/nestabur/AngularWorkshop
 cd AngularWorkshop
 npm install -g gulp
 npm install
